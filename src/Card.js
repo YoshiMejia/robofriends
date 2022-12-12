552022089f683cf7d35a109ec44928f28d5a97bb
Yoshi <yoshimejia00@gmail.com>
2022-12-12 10:08:46 -0500

    card component init commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+    return(
+            <div className="bg-light-green br3 pa3 ma2 grow bw2 shadow-5">
+                <img src="https://robohash.org/test?200x200" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Jane Doe</p>
+            </div>
+    )
+}
+
+export default Card;
